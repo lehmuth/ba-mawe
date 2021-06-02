45febc3dcf1caf0cb88f259727e666c5435549ad
lehmuth <lehmuth@doca.codes>
2021-06-02 23:09:55 +0000

    Fixed some button bugs

--- src/components/bold-button.js
+++ src/components/bold-button.js
@@ -1,14 +1,20 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
+        :host {
+            display: inline-block;
+            box-sizing: boder-box;
+        }    
+
         #container {
+            min-width: 100%;
             background-color: #000;
+            display: inline-block;
             padding: 10px;
             color: #fff;
             text-decoration: none;
             text-transform: uppercase;
             box-sizing: border-box;
-            margin: 10px;
         }
 
         .clickable {
@@ -16,27 +22,27 @@ template.innerHTML = `
         }
 
         #label {
-
         }
 
-        .bracket {
+        #bracket {
+            
+        }
 
+        .unlinked > #bracket {
+            display: none;
         }
     </style>
     <a id="container">
         <span id="label">
             <slot>Label</slot>
         </span>
+        <span id="bracket">&gt;</span>
     </a>
 `;
 
-const bracket = document.createElement('span');
-bracket.setAttribute('class', 'bracket');
-bracket.innerHTML = '>';
-
 class BoldButton extends HTMLElement {
     static get observedAttributes () {
-        return ['href'];
+        return ['href', 'onclick'];
     }
 
     constructor() {
@@ -55,7 +61,7 @@ class BoldButton extends HTMLElement {
     }
 
     set href(value) {
-    this.setAttribute('href', value);
+        this.setAttribute('href', value);
     }
 
     attributeChangedCallback(name, oldVal, newVal) {
@@ -63,6 +69,10 @@ class BoldButton extends HTMLElement {
             switch(name) {
                 case 'href': 
                     this.href = newVal;
+                    this.clickable = newVal ? true : false;
+                    break;
+                case 'onclick':
+                    this.clickable = newVal ? true : false;
                     break;
             }
             this.render();
@@ -70,21 +80,18 @@ class BoldButton extends HTMLElement {
     }
 
     render() {
-        const bracketNodes = this.container.getElementsByClassName('bracket');
         if(!this.href) {
-            if(bracketNodes) {
-                while(bracketNodes.length > 0) {
-                    bracketNodes[0].parentNode.removeChild(bracketNodes[0]);
-                }
-            }
             this.container.removeAttribute('href');
-            this.container.classList.remove('clickable');
+            this.container.classList.add('unlinked');
         } else {
-            if(!bracketNodes || bracketNodes.length === 0) {
-                this.container.appendChild(bracket);
-            }
             this.container.setAttribute('href', this.href);
+            this.container.classList.remove('unlined');
+        }
+        
+        if(this.clickable) {
             this.container.classList.add('clickable');
+        } else {
+            this.container.classList.remove('clickable');
         }
     }
 }
