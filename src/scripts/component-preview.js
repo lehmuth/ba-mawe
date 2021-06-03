f97305be6e7d4e33cdd9eb4539ac6fdd3da80e67
lehmuth <lehmuth@doca.codes>
2021-06-03 12:04:58 +0000

    Started parallax effect

--- src/scripts/component-preview.js
+++ src/scripts/component-preview.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const progressBar = document.querySelector('#bar-1');
 
@@ -12,3 +13,38 @@ function subtract(value) {
 function reset() {
     progressBar.reset();
 }
+
+const parallax = document.querySelector('#parallax');
+
+// element speed in relation to scroll speed 
+// { 2 }    => moving twice as fast as default in the same direction
+// (1;inf)  => moving faster than default elements in same direction (coming from top to leave on default position)
+// { 1 }    => moving with exactly the same speed as scrolling -> never visible
+// (0;1)    => moving slower than default elements in same direction
+// { 0 }    => moving with same speed as default elements -> no result
+// (-inf;0) => moving faster than default elements in opposite direction
+// { -1 }   => moving twice as fast as default elements in opposite direction
+const speedRatio = -0.5;
+const range = 30;
+
+function render () {
+    // Load window bounds and scroll direction
+    const winTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const winBottom = winTop + window.innerHeight;
+
+    // load element bounds
+    const elemTop = parallax.offsetTop;
+    const offset = Math.floor((winBottom - elemTop) * speedRatio);
+    const elemBottom = elemTop + parallax.offsetHeight;
+
+    // check if element is visible
+    const visible = (winBottom + range) - (elemTop + offset) >= 0 && (winTop - range) - (elemBottom + offset) < 0;
+
+    console.log(winTop, elemBottom, visible);
+
+    if (visible) {
+        parallax.style.transform = `translate(0,${offset}px)`;
+    }
+}
+
+setInterval(render, 1000 / 120);
